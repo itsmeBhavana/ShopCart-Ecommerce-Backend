bd5f423e2fa68872c011b67620f373f7654a36df
itsmeBhavana <bhavana.matavalam@gmail.com>
2024-11-20 10:53:07 -0800

    Added basic in-memory db based service layer

--- src/controllers/product_controller.js
+++ src/controllers/product_controller.js
@@ -1,23 +1,50 @@
 const { StatusCodes, ReasonPhrases } = require("http-status-codes");
+const {
+  createProduct,
+  getProducts,
+  getProduct,
+} = require("../services/product_service");
 
 function create_product(req, res) {
   try {
-    //db processing
+    const response = createProduct(req.body);
     return res.status(StatusCodes.CREATED).json({
       success: true,
       error: {},
       message: ReasonPhrases.CREATED,
-      data: {
-        id: Math.random() * 20,
-        title: req.body.title,
-        category: req.body.category,
-        author: req.body.author,
-        genre: req.body.genre,
-      },
+      data: response,
     });
   } catch (err) {
     console.log("Something went wrong", err);
   }
 }
 
-module.exports = { create_product };
+function get_products(req, res) {
+  try {
+    const response = getProducts();
+    res.status(StatusCodes.OK).json({
+      success: true,
+      error: {},
+      message: "Successfully fetched the products",
+      data: response,
+    });
+  } catch (err) {
+    console.log("Something went wrong", err);
+  }
+}
+
+function get_product(req, res) {
+  try {
+    const response = getProduct(req.params.id);
+    res.status(StatusCodes.OK).json({
+      success: true,
+      error: {},
+      message: "Successfully fetched the productb",
+      data: response,
+    });
+  } catch (err) {
+    console.log("Something went wrong", err);
+  }
+}
+
+module.exports = { create_product, get_products, get_product };
--- src/routes/v1/product_router.js
+++ src/routes/v1/product_router.js
@@ -1,15 +1,17 @@
 const express = require("express");
 
 const productRouter = express.Router();
-const { create_product } = require("../../controllers/product_controller");
+const {
+  create_product,
+  get_products,
+  get_product,
+} = require("../../controllers/product_controller");
 const {
   createProductValidator,
 } = require("../../middlewares/product_middlewares");
 
-productRouter.get("/", (req, res) => {
-  return res.json({ products: [] });
-}); //mapping a route to a controller
-
 productRouter.post("/", createProductValidator, create_product);
+productRouter.get("/", get_products);
+productRouter.get("/:id", get_product);
 
 module.exports = productRouter;
